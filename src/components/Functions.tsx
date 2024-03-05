e35c41e304c01f1017d52e644bcf82534dc44782
Florian Erwerth <erwerthflorian@outlook.de>
2024-03-05 09:31:29 +0100

    further adjustments

--- src/components/Functions.tsx
+++ src/components/Functions.tsx
@@ -17,7 +17,7 @@ export const Functions = () => {
   const getImageClass = useCallback(
     (index: number) => {
       const base =
-        "h-[600px] max-lg:h-[75vh] transition-all duration-300 hover:opacity-100 lg:scale-100 lg:hover:scale-105 cursor-pointer";
+        "h-[75vh] max-lg:h-[75vh] transition-all duration-300 hover:opacity-100 lg:scale-100 lg:hover:scale-105 cursor-pointer";
       if (index === selectedIndex) {
         return `${base} opacity-100 lg:scale-105`;
       }
@@ -104,15 +104,27 @@ export const Functions = () => {
         style={{ scrollbarWidth: "none" }}
         className="grid lg:justify-between grid-cols-[max-content_max-content_max-content] px-12 max-lg:overflow-x-auto max-lg:overflow-y-hidden"
       >
-        <button className="h-[590px] overflow-hidden" ref={pic1Ref} onClick={() => handleSelect(0)}>
+        <button
+          className="h-[calc(75vh-40px)] overflow-hidden"
+          ref={pic1Ref}
+          onClick={() => handleSelect(0)}
+        >
           <div id="img1" className="relative -top-10" />
           <img className={getImageClass(0)} src={push_workout} alt="" />
         </button>
-        <button className="h-[590px] overflow-hidden" ref={pic2Ref} onClick={() => handleSelect(1)}>
+        <button
+          className="h-[calc(75vh-40px)] overflow-hidden"
+          ref={pic2Ref}
+          onClick={() => handleSelect(1)}
+        >
           <img className={getImageClass(1)} src={graph} alt="graph" />
           <div id="img2" />
         </button>
-        <button className="h-[590px] overflow-hidden" ref={pic3Ref} onClick={() => handleSelect(2)}>
+        <button
+          className="h-[calc(75vh-40px)] overflow-hidden"
+          ref={pic3Ref}
+          onClick={() => handleSelect(2)}
+        >
           <img className={getImageClass(2)} src={overview} alt="" />
           <div id="img3" />
         </button>
