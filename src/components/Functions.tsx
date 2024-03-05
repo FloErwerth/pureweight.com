44d2835f51ec767fc1d0fc200e1e507248593c46
Florian Erwerth <erwerthflorian@outlook.de>
2024-03-05 09:29:21 +0100

    fix scroll pos

--- src/components/Functions.tsx
+++ src/components/Functions.tsx
@@ -17,7 +17,7 @@ export const Functions = () => {
   const getImageClass = useCallback(
     (index: number) => {
       const base =
-        "lg:h-[600px] max-lg:h-[75vh] transition-all duration-300 hover:opacity-100 lg:scale-100 lg:hover:scale-105 cursor-pointer";
+        "h-[600px] max-lg:h-[75vh] transition-all duration-300 hover:opacity-100 lg:scale-100 lg:hover:scale-105 cursor-pointer";
       if (index === selectedIndex) {
         return `${base} opacity-100 lg:scale-105`;
       }
@@ -74,7 +74,11 @@ export const Functions = () => {
       if (document.documentElement.clientWidth < 1024) {
         switch (index) {
           case 0:
-            pic1Ref.current?.scrollIntoView({ behavior: "smooth", inline: "center", block: "end" });
+            pic1Ref.current?.scrollIntoView({
+              behavior: "smooth",
+              inline: "center",
+              block: "end",
+            });
             break;
           case 1:
             pic2Ref.current?.scrollIntoView({ behavior: "smooth", inline: "center", block: "end" });
@@ -100,14 +104,17 @@ export const Functions = () => {
         style={{ scrollbarWidth: "none" }}
         className="grid lg:justify-between grid-cols-[max-content_max-content_max-content] px-12 max-lg:overflow-x-auto max-lg:overflow-y-hidden"
       >
-        <button ref={pic1Ref} id="img1" onClick={() => handleSelect(0)}>
+        <button className="h-[590px] overflow-hidden" ref={pic1Ref} onClick={() => handleSelect(0)}>
+          <div id="img1" className="relative -top-10" />
           <img className={getImageClass(0)} src={push_workout} alt="" />
         </button>
-        <button ref={pic2Ref} id="img2" onClick={() => handleSelect(1)}>
+        <button className="h-[590px] overflow-hidden" ref={pic2Ref} onClick={() => handleSelect(1)}>
           <img className={getImageClass(1)} src={graph} alt="graph" />
+          <div id="img2" />
         </button>
-        <button ref={pic3Ref} id="img3" onClick={() => handleSelect(2)}>
+        <button className="h-[590px] overflow-hidden" ref={pic3Ref} onClick={() => handleSelect(2)}>
           <img className={getImageClass(2)} src={overview} alt="" />
+          <div id="img3" />
         </button>
       </div>
     </>
